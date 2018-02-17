6dee153ed77dbc91360463e9d83894db9e46182e
maduck <utti+github@uttisoft.de>
2018-02-17 09:17:06 +0100

    removed code that has a good library already

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import colorsys
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -18,32 +19,11 @@ def rescale(value, in_min, in_max, out_min, out_max):
     return (value - in_min) * (out_range / in_range) + out_min
 
 
-def hsv_to_rgb(h, s, v):
-    if s == 0.0:
-        v *= 255
-        return [v, v, v]
-    i = int(h * 6.)  # XXX assume int() truncates!
-    f = (h * 6.) - i
-    p, q, t = int(255 * (v * (1. - s))), int(255 * (v * (1. - s * f))), int(255 * (v * (1. - s * (1. - f))))
-    v *= 255
-    i %= 6
-    if i == 0:
-        return [v, t, p]
-    if i == 1:
-        return [q, v, p]
-    if i == 2:
-        return [p, v, t]
-    if i == 3:
-        return [p, q, v]
-    if i == 4:
-        return [t, p, v]
-    if i == 5:
-        return [v, p, q]
-
-
 def get_colors(count):
     colors = []
     for i in range(0, 360, 360 // count):
-        colors.append(hsv_to_rgb(i / 360, 1, 1))
+        rgb = colorsys.hsv_to_rgb(i / 360, 1, 1)
+        colors.append([int(255*i) for i in rgb])
     colors.reverse()
+
     return colors
